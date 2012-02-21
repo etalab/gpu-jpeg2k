3c376e8bb9ef460bc69c454ed915f7caa8863870
miloszc <miloszc@man.poznan.pl>
2012-02-21 15:47:20 +0000

    Update.

--- gpu_jpeg2k/tier1/bpc/gpu_convert.cu
+++ gpu_jpeg2k/tier1/bpc/gpu_convert.cu
@@ -67,7 +67,7 @@ __device__ void preprocessing(unsigned int cxds[][Code_Block_Size_X], unsigned i
 	}
 }
 
-template <char Code_Block_Size_X>
+/*template <char Code_Block_Size_X>
 __global__ void g_convert(CodeBlockAdditionalInfo *infos, unsigned int *g_icxds, unsigned char *g_ocxds, const int maxOutLength) {
 	__shared__ unsigned int cxds[Code_Block_Size_X * Code_Block_Size_X];
 //	__shared__ unsigned int pos[Code_Block_Size_X][Code_Block_Size_X];
@@ -123,6 +123,113 @@ __global__ void g_convert(CodeBlockAdditionalInfo *infos, unsigned int *g_icxds,
 	if(TID == 0) {
 		info->magconOffset = curr_pos;
 	}
+}*/
+
+template <char Code_Block_Size_X, char Pass, char Bitpos>
+__device__ void pass(CodeBlockAdditionalInfo *infos, unsigned int cxds[][Code_Block_Size_X + BORDER], unsigned char ocxds[(Code_Block_Size_X * 4) * (Code_Block_Size_X * 4)], unsigned int pos[][Code_Block_Size_X],
+		unsigned int par_sum[Code_Block_Size_X], unsigned int sum[Code_Block_Size_X], unsigned int &offset, unsigned char *g_ocxds, const int maxOutLength) {
+	ocxds[TIDY * Code_Block_Size_X * 4 + TIDX * 4 + 0] = 0;
+	ocxds[TIDY * Code_Block_Size_X * 4 + TIDX * 4 + 1] = 0;
+	ocxds[TIDY * Code_Block_Size_X * 4 + TIDX * 4 + 2] = 0;
+	ocxds[TIDY * Code_Block_Size_X * 4 + TIDX * 4 + 3] = 0;
+	if(TIDY == 0) {
+		par_sum[TIDX] = 0;
+//#pragma unroll 16
+		for(int i = BORDER; i < Code_Block_Size_X + BORDER; ++i) {
+			par_sum[TIDX] += ((cxds[X][i] & Pass) >> Bitpos) * (cxds[X][i] & CXD_COUNTER);
+		}
+	}
+	__syncthreads();
+
+	if(TIDY == 0) {
+		sum[TIDX] = offset;
+//		if(offset > 30000)
+//		printf("%x	%d\n", sum[TIDX], TIDX);
+		for(int i = 0; i < TIDX; ++i) {
+			sum[TIDX] += par_sum[i];
+		}
+	}
+	__syncthreads();
+
+	if(TIDY == 0) {
+//		printf("%x	%d\n", sum[TIDX], TIDX);
+		unsigned int curr_pos = sum[TIDX];
+//		if(curr_pos > 3000)
+//		printf("%x 	%d	%d\n", curr_pos, TIDX, 0);
+//#pragma unroll 16
+		for(int i = 0; i < Code_Block_Size_X; ++i) {
+			curr_pos += ((cxds[X][i] & Pass) >> Bitpos) * (cxds[X][i] & CXD_COUNTER);
+			pos[TIDX][i] = curr_pos;
+//			if(pos[TIDX][i] > 3000)
+//				printf("%d 	%d	%d\n", pos[TIDX][i], TIDX, i);
+		}
+	}
+	__syncthreads();
+//	if((TIDY == (Code_Block_Size_X - 1)) && (TIDX == (Code_Block_Size_X - 1))) {
+//		printf("size %d pos %d\n", offset, pos[TIDY][TIDX]);
+//	}
+//	__syncthreads();
+
+	for(unsigned char k = 0; k < (cxds[Y][X] & CXD_COUNTER) * ((cxds[Y][X] & Pass) >> Bitpos); ++k) {
+		ocxds[pos[TIDY][TIDX] + k] =
+//		g_ocxds[blockIdx.x * maxOutLength * 4 + pos[TIDY][TIDX] + k] =
+				(((cxds[Y][X] >> (D1_BITPOS - k * 6)) & 0x1) << 5) | ((cxds[Y][X] >> (CX1_BITPOS - k * 6)) & 0x1f);
+//		printf("%x	%x	%d	%d	%d	%d	%d\n", cxds[Y][X], Pass, ((cxds[Y][X] >> (D1_BITPOS - k * 6)) & 0x1), ((cxds[Y][X] >> (CX1_BITPOS - k * 6)) & 0x1f), blockIdx.x * maxOutLength * 4 + pos[TIDY][TIDX] + k, TIDY, TIDX);
+//		printf("%x	%d	%d\n", ocxds[pos[TIDY][TIDX] + k], TIDY, TIDX);
+	}
+	__syncthreads();
+//	printf("write %d	%d	%d	%d\n", blockIdx.x * maxOutLength * 4 + offset + TIDY * Code_Block_Size_X + TIDX, offset, TIDY, TIDX);
+	g_ocxds[blockIdx.x * maxOutLength * 4 + offset + TIDY * Code_Block_Size_X * 4 + TIDX * 4 + 0] = ocxds[TIDY * Code_Block_Size_X * 4 + TIDX * 4 + 0];
+	g_ocxds[blockIdx.x * maxOutLength * 4 + offset + TIDY * Code_Block_Size_X * 4 + TIDX * 4 + 1] = ocxds[TIDY * Code_Block_Size_X * 4 + TIDX * 4 + 1];
+	g_ocxds[blockIdx.x * maxOutLength * 4 + offset + TIDY * Code_Block_Size_X * 4 + TIDX * 4 + 2] = ocxds[TIDY * Code_Block_Size_X * 4 + TIDX * 4 + 2];
+	g_ocxds[blockIdx.x * maxOutLength * 4 + offset + TIDY * Code_Block_Size_X * 4 + TIDX * 4 + 3] = ocxds[TIDY * Code_Block_Size_X * 4 + TIDX * 4 + 3];
+	__syncthreads();
+
+//	if(TID == 0) printf("offset %d	%d	%d\n", offset, TIDY, TIDX);
+
+	if((TIDY == (Code_Block_Size_X - 1)) && (TIDX == (Code_Block_Size_X - 1))) {
+		offset = pos[TIDY][TIDX] + ((cxds[Y][X] & Pass) >> Bitpos) * (cxds[Y][X] & CXD_COUNTER);
+//		printf("size %d pos %d\n", offset, pos[TIDY][TIDX]);
+	}
+	__syncthreads();
+}
+
+template <char Code_Block_Size_X>
+__global__ void g_convert(CodeBlockAdditionalInfo *infos, unsigned int *g_icxds, unsigned char *g_ocxds, const int maxOutLength) {
+	__shared__ unsigned int cxds[Code_Block_Size_X + BORDER][Code_Block_Size_X + BORDER];
+	__shared__ unsigned char ocxds[(Code_Block_Size_X * Code_Block_Size_X * 16)];
+	__shared__ unsigned int pos[Code_Block_Size_X][Code_Block_Size_X];
+	__shared__ unsigned int par_sum[Code_Block_Size_X];
+	__shared__ unsigned int sum[Code_Block_Size_X];
+	__shared__ unsigned int offset;
+	CodeBlockAdditionalInfo *info = &(infos[blockIdx.x]);
+	int size = Code_Block_Size_X * Code_Block_Size_X;
+
+	cxds[Y][X] = g_icxds[blockIdx.x * maxOutLength + TIDY * Code_Block_Size_X + TIDX];
+//	printf("%x	%d	%d\n", ((cxds[Y][X] & SPP) >> SPP_BITPOS) * (cxds[Y][X] & CXD_COUNTER), TIDY, TIDX);
+	if(TID == 0) offset = 0;
+	if(TIDY == 0) cxds[X][0] = 0;
+	__syncthreads();
+
+	pass<Code_Block_Size_X, CUP, CUP_BITPOS>(info, cxds, ocxds, pos, par_sum, sum, offset, g_ocxds, maxOutLength);
+	__syncthreads();
+
+	for(unsigned char i = 1; i < info->significantBits; ++i) {
+		cxds[Y][X] = g_icxds[blockIdx.x * maxOutLength + i * size + TIDY * Code_Block_Size_X + TIDX];
+//		if(TIDY == 0) cxds[TIDX][0] = 0;
+		__syncthreads();
+		if(TID == 0) printf("%d) offset %d	%d	%d\n", i, offset, TIDY, TIDX);
+		pass<Code_Block_Size_X, SPP, SPP_BITPOS>(info, cxds, ocxds, pos, par_sum, sum, offset, g_ocxds, maxOutLength);
+		__syncthreads();
+		if(TID == 0) printf("%d) offset %d	%d	%d\n", i, offset, TIDY, TIDX);
+		pass<Code_Block_Size_X, MRP, MRP_BITPOS>(info, cxds, ocxds, pos, par_sum, sum, offset, g_ocxds, maxOutLength);
+		__syncthreads();
+		if(TID == 0) printf("%d) offset %d	%d	%d\n", i, offset, TIDY, TIDX);
+		pass<Code_Block_Size_X, CUP, CUP_BITPOS>(info, cxds, ocxds, pos, par_sum, sum, offset, g_ocxds, maxOutLength);
+		__syncthreads();
+	}
+
+	if(TID == 0) info->magconOffset = offset;
 }
 
 void convert(dim3 gridDim, dim3 blockDim, CodeBlockAdditionalInfo *infos, unsigned int *g_icxds, unsigned char *g_ocxds, const int maxOutLength)
