77bb02a10909138dd10876eaebc2d71eea97b6fa
miloszc <miloszc@man.poznan.pl>
2013-07-24 14:17:18 +0000

    Add license to trunk.

--- gpu_jpeg2k/klt/gs.h
+++ gpu_jpeg2k/klt/gs.h
@@ -1,3 +1,22 @@
+/* 
+Copyright 2009-2013 Poznan Supercomputing and Networking Center
+
+Authors:
+Milosz Ciznicki miloszc@man.poznan.pl
+
+GPU JPEG2K is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+GPU JPEG2K is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with GPU JPEG2K. If not, see <http://www.gnu.org/licenses/>.
+*/
 /**
  * @file gs.h
  *
